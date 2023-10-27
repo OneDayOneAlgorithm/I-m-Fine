578f6f1b2689d0ce8bd160e1cb6a16a5a644cba4
kieanupark <kieanupark@gmail.com>
2023-10-27 17:57:35 +0900

    Feat: 카드 만드는 중

--- finetune-web/src/index.js
+++ finetune-web/src/index.js
@@ -11,6 +11,13 @@ root.render(
   </React.StrictMode>
 );
 
+const imgCard = ReactDOM.createRoot(document.getElementById('imgCard'));
+imgCard.render(
+  <React.StrictMode>
+    <custom />
+  </React.StrictMode>
+);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
