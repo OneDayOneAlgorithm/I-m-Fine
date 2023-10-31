a9df15149e5c03778ddec5a8d415f36e65081c9e
kieanupark <kieanupark@gmail.com>
2023-10-31 17:48:52 +0900

    Feat: 모델링 페이지 완료 및 서버 통신 구현 중

--- finetune-web/src/Modelify.js
+++ finetune-web/src/Modelify.js
@@ -1,18 +1,69 @@
-import React from 'react';
-import { Link, useLocation } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import {useLocation, useNavigate } from "react-router-dom";
 
 const Modelify = () => {
   const location = useLocation();
-  const data = location.state.data;
+  const navigate = useNavigate();
+  const model = location.state.data;
   const sliderValues = location.state.sliderValues;
-  
+  const text = 'MODELING...';
+  const delay = 500;
+
+  const [data, setData] = useState([]);
+
+  const [currentText, setCurrentText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (currentIndex <= text.length) {
+      const timeout = setTimeout(() => {
+        setCurrentText(prevText => prevText + text[currentIndex]);
+        setCurrentIndex(prevIndex => prevIndex + 1);
+      }, delay);
+      
+      return () => clearTimeout(timeout);
+    }else {
+      setCurrentText('');
+      setCurrentIndex(0);
+    }
+  }, [currentIndex, delay, text]);
+
+  // Function to fetch data using Axios
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://k9d109.p.ssafy.io/api/cat");
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  // Call fetchData on component mount
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <h1>{data}</h1>
-      <h2>{sliderValues}</h2>
-      <Link to="/">
-        <h1>ddd</h1>
-      </Link>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <div style={{ height: '80vh',width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        {/* <h1>{model}</h1>
+        <h2>{sliderValues}</h2> */}
+        {data.length !== 0 ? (
+          console.log(data),
+          data.map((post) => (
+            <li>{post}</li>
+          )),
+          navigate('/home')
+        ) : (
+          <div style={{ pointerEvents: 'none' }}>
+            <img class='unselectable' src="loading_no_bg.gif" alt="로딩바" autoplay />
+          </div>
+        )}
+      </div>
+      <div style={{ height: '20vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'flex-start' }}>
+        <h1 style={{ color: 'white', fontSize: '5em', fontFamily: 'sebangFont' }}>{currentText}</h1>
+      </div>
     </div>
   );
 };
