ab5c0b70267c2ae70e3dc4de0a7d38e2cdec1f43
이현근 <leehyunk6310@naver.com>
2023-10-31 15:06:10 +0900

    ft: docker-compose.yaml 작성

--- finetune-back/src/main.py
+++ finetune-back/src/main.py
@@ -1,15 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 app = FastAPI() # 인스턴스 생성
+router = APIRouter()  # APIRouter 인스턴스 생성
 
-@app.get("/") # get method로 '/'에 해당하는  생성
+
+@router.get("/") # get method로 '/'에 해당하는  생성
 def root():
     return {'Hello':'World!'} 
 
-@app.get("/cat")
+@router.get("/cat")
 def cat():
     return "Meow"
 
-@app.get("/dog")
+@router.get("/dog")
 def dog():
     return "Barkbark"
+
+app.include_router(router, prefix="/api")  # "/api" 접두사와 함께 router를 app에 포함
