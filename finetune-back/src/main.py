42626fe7d421015ce7eb59a1511f1ffac207a313
이현근 <leehyunk6310@naver.com>
2023-11-01 11:14:31 +0900

    ft: add cors

--- finetune-back/src/main.py
+++ finetune-back/src/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI() # 인스턴스 생성
 router = APIRouter()  # APIRouter 인스턴스 생성
+
+origins = [
+    # "http://192.168.0.13:3000", # url을 등록해도 되고
+    "*" # private 영역에서 사용한다면 *로 모든 접근을 허용할 수 있다.
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True, # cookie 포함 여부를 설정한다. 기본은 False
+    allow_methods=["*"],    # 허용할 method를 설정할 수 있으며, 기본값은 'GET'이다.
+    allow_headers=["*"],	# 허용할 http header 목록을 설정할 수 있으며 Content-Type, Accept, Accept-Language, Content-Language은 항상 허용된다.
+)
 
 
 @router.get("/") # get method로 '/'에 해당하는  생성
