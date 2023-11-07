f83d8065e3d5a6cfbdfb793647a9f702e2a19261
이현근 <leehyunk6310@naver.com>
2023-11-07 14:00:53 +0900

    ft: colab gpt 함수 연동

--- finetune-back/src/main.py
+++ finetune-back/src/main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI, APIRouter, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+import requests
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import os
 
-
 app = FastAPI() # 인스턴스 생성
 app.mount("/static", StaticFiles(directory="static"), name="static")
 router = APIRouter()  # APIRouter 인스턴스 생성
+colab_url = ""
 templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "templates"))
 
 origins = [
@@ -19,7 +20,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True, # cookie 포함 여부를 설정한다. 기본은 False.
+    allow_credentials=True, # cookie 포함 여부를 설정한다. 기본은 False
     allow_methods=["*"],    # 허용할 method를 설정할 수 있으며, 기본값은 'GET'이다.
     allow_headers=["*"],	# 허용할 http header 목록을 설정할 수 있으며 Content-Type, Accept, Accept-Language, Content-Language은 항상 허용된다.
 )
@@ -32,12 +33,22 @@ async def micro(request: Request):
 def root():
     return {'Hello':'World!'} 
 
-@router.get("/cat")
-def cat():
-    return "Meow"
+# 코랩 주소 업데이트
+@router.post("/set-url")
+def setUrl(url: str): 
+    global colab_url
+    colab_url = url
+    return colab_url
 
-@router.get("/dog")
-def dog():
-    return "Barkbark"
+@router.get("/get-url")
+def getUrl():
+    return colab_url
+
+#gpt 모델 가중치 변경해서 데이터 반환
+@router.post("/gpt")
+async def call_colab_function(request: Request):
+    json_data = await request.json()
+    response = requests.post(colab_url, json=json_data)
+    return response.text
 
 app.include_router(router, prefix="/api")  # "/api" 접두사와 함께 router를 app에 포함
