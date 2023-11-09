b8188fb736e447eab0cd45cf1c6e4f831317f390
kieanu <kieanupark@gmail.com>
2023-11-09 21:04:59 +0900

    Feat: 인풋 텍스트 추가 POST 추가

--- finetune-web/src/Parameter.js
+++ finetune-web/src/Parameter.js
@@ -10,30 +10,30 @@ function MySlider({ paramName, paramDesc, onSliderChange, modelName }) {
 
   const sliderStyle = {
     marginTop: "1em", // 여기에 마진 값을 설정하세요.
-    display: 'flex',
+    display: "flex",
     // flex: '2',
-    marginLeft: '10em',
-    marginRight: '10em',
-    alignItems: 'center',
+    marginLeft: "10em",
+    marginRight: "10em",
+    alignItems: "center",
   };
 
   const paramStyle = {
     fontFamily: "bmFont",
-    fontSize: '3.5em',
-    color: 'white',
-    flex: '1',
-    marginRight: '2em',
-  }
+    fontSize: "3.5em",
+    color: "white",
+    flex: "1",
+    marginRight: "2em",
+  };
 
   const sliderTextStyle = {
     fontFamily: "bmFont",
-    color: 'white',
-    textAlign: 'center',
-    marginTop: '1.5em',
-  }
+    color: "white",
+    textAlign: "center",
+    marginTop: "1.5em",
+  };
 
   return (
-    <div style={{ marginBottom: '3em' }}>
+    <div style={{ marginBottom: "3em" }}>
       <div style={sliderStyle}>
         <div style={paramStyle}>{paramName}</div>
         <div>
@@ -60,22 +60,32 @@ const Parameter = () => {
   const location = useLocation();
   const data = location.state.data;
   const [isVisible, setIsVisible] = useState(false);
+  const [inputText, setInputText] = useState("");
 
-  const curIdx = data === 'GPT' ? 0
-    : data === 'LLAMA' ? 1 : 2;
+  const curIdx = data === "GPT" ? 0 : data === "LLAMA" ? 1 : 2;
 
-  const params = [['EPS', 'ATTN', 'MLP'], ['L1', 'L2', 'L3'], ['S1', 'S2', 'S3', 'S4', 'S5']];
-
-  const paramDescs = [
-    ['높을수록 데이터 의존성 증가', '높을수록 단어 간 순서 의존성 증가', '높을수록 단어간 연관성 증가'],
-    ['설명1', '설명2', '설명3'],
-    ['설명1', '설명2', '설명3', '설명4', '설명5']
+  const params = [
+    ["EPS", "ATTN", "MLP"],
+    ["L1", "L2", "L3"],
+    ["S1", "S2", "S3", "S4", "S5"],
   ];
 
-  const [sliderValues, setSliderValues] = useState(new Array(params[curIdx].length).fill(1.0));
+  const paramDescs = [
+    [
+      "높을수록 데이터 의존성 증가",
+      "높을수록 단어 간 순서 의존성 증가",
+      "높을수록 단어간 연관성 증가",
+    ],
+    ["설명1", "설명2", "설명3"],
+    ["설명1", "설명2", "설명3", "설명4", "설명5"],
+  ];
+
+  const [sliderValues, setSliderValues] = useState(
+    new Array(params[curIdx].length).fill(1.0)
+  );
 
   const handleSliderChange = (paramName, newValue) => {
-    setSliderValues(prevValues => {
+    setSliderValues((prevValues) => {
       const index = params[curIdx].indexOf(paramName);
       if (index !== -1) {
         const newValues = [...prevValues];
@@ -102,18 +112,21 @@ const Parameter = () => {
   const topPaneStyle = {
     flex: 1, // 1:2 비율로 나누기 위해 flex 속성 사용
     display: "flex",
+    flexDirection: "column",
     alignItems: "center",
+    justifyContent: "center",
     height: "100%", // 부모 컨테이너의 높이를 100%로 설정합니다.
     // overflow: "auto", // 내용이 넘칠 경우 스크롤 가능하도록 설정
   };
 
   const bottomPaneStyle = {
     flex: 2,
-    width: '100%',
+    width: "100%",
     flexDirection: "column",
     overflow: "auto",
-    paddingTop: '1.2em',
-    opacity: isVisible ? 1 : 0, transition: 'opacity 1s'
+    paddingTop: "1.2em",
+    opacity: isVisible ? 1 : 0,
+    transition: "opacity 1s",
   };
 
   const headingStyle = {
@@ -130,7 +143,7 @@ const Parameter = () => {
   return (
     <div style={{ display: "flex", width: "100%", alignItems: "center" }}>
       <div style={arrowStyle}>
-        <Link to="/">
+        <Link to="/choose">
           <img
             src="/arrow_img.png"
             alt="좌측 화살표"
@@ -141,15 +154,33 @@ const Parameter = () => {
       <div style={containerStyle}>
         <div style={topPaneStyle}>
           <h1 style={headingStyle}>{data} 파라미터 조정</h1>
+          {data !== "Stable Diffusion" && (
+            <input
+              type="text"
+              value={inputText}
+              onChange={(e) => setInputText(e.target.value)}
+              placeholder="텍스트를 입력하시면 변화된 결과를 보여드립니다."
+              style={{
+                width: "100%",
+                boxSizing: "border-box",
+                marginTop: "1em",
+              }}
+            />
+          )}
         </div>
         <div class="scroll-hide" style={bottomPaneStyle}>
           {params[curIdx].map((param, index) => (
-            <MySlider modelName={data} paramName={param} paramDesc={paramDescs[curIdx][index]} onSliderChange={handleSliderChange}/>
+            <MySlider
+              modelName={data}
+              paramName={param}
+              paramDesc={paramDescs[curIdx][index]}
+              onSliderChange={handleSliderChange}
+            />
           ))}
         </div>
       </div>
       <div style={arrowStyle}>
-        <Link to="/Modelify" state={{sliderValues, data}}>
+        <Link to="/Modelify" state={{ sliderValues, data, inputText }}>
           <img src="/arrow_img.png" alt="우측 화살표" />
         </Link>
       </div>
